7e756c00f80c8943cd9b55a945abe8ffb73f99b8
Dappner <nicklasastorian@googlemail.com>
2025-03-19 13:51:25 -0400

    More

--- track_testing.py
+++ track_testing.py
@@ -1,6 +1,8 @@
 from utils.brick import wait_ready_sensors, Motor
 import time
 
+MOTOR_DPS = 90
+
 motor_left = Motor("D")
 motor_right = Motor("A")
 
@@ -12,13 +14,23 @@ def turn_90():
     motor_left.reset_encoder()
     motor_right.reset_encoder()
 
-    motor_left.set_dps(90)
-    motor_right.set_position(-90)
+    motor_left.set_dps(MOTOR_DPS)
+    motor_right.set_dps(-MOTOR_DPS)
 
     time.sleep(2)
 
-    reset_sensors()
+    reset_motors()
     return
+
+
+def advance_one():
+    print("advancing one field")
+    motor_left.set_dps(MOTOR_DPS)
+    motor_right.set_dps(MOTOR_DPS)
+
+    time.sleep(2)
+
+    reset_motors()
 
 
 def main():
@@ -28,7 +40,7 @@ def main():
     turn_90()
 
 
-def reset_sensors():
+def reset_motors():
     motor_left.set_power(0)
     motor_right.set_power(0)
 
@@ -38,5 +50,5 @@ if __name__ == "__main__":
     try:
         main()
     except Exception:
-        reset_sensors()
+        reset_motors()
         print("whoops!")
